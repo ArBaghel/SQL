db70721c2324dc01b81b2bc305dce88a11f6a905
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-06-19 01:59:26 +0530

    Time: 399 ms (89.08%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,3 +1,11 @@
 # Write your MySQL query statement below
 select p.firstname, p.lastname,a.city,a.state
 from person p left join  address a on p.personId=a.personId;
+
+-- import pandas as pd
+
+-- def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+--     result=pd.merge(person,address,on= 'personId',how='left')
+--     result=result[[ 'firstName','lastName','city','state' ]]
+--     return result
+    
