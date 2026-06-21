bda65cd39a08b1e16c2b71a49e27bc6d8ffbdd69
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-06-22 03:19:16 +0530

    Time: 268 ms (90.79%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -2,3 +2,11 @@
 select (
     select  distinct salary  from employee  order by salary desc limit 1 offset 1
   ) as SecondHighestSalary  ;
+
+-- import pandas as pd
+
+-- def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+--     s=employee['salary'].drop_duplicates().nlargest(2)
+--     if len(s)<2:return pd.DataFrame({'SecondHighestSalary':[None]})
+--     return pd.DataFrame({'SecondHighestSalary':[s.iloc[-1]]})
+    
