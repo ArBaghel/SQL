e7b24bde79f8043e0083c488b3d3ab561095b2e7
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-07-19 22:23:35 +0530

    Time: 259 ms (81.62%) | Memory: 0B (100.00%) - LeetSync

---
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select 
+    id,movie,description,rating 
+from 
+    cinema 
+where 
+    id & 1=1 
+and 
+    description !='boring' 
+order by 
+    rating desc;
