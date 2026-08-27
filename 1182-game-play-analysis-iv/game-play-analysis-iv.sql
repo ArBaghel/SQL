0c67d65c1012ddc2aab74a3a9dd8793dacd8c428
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-08-27 22:49:44 +0530

    Time: 643 ms (41.50%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,7 +1,7 @@
--- Write your PostgreSQL query statement below
-select round(count(a.player_id)::decimal/count(*),2) as fraction from
-(
-    select player_id,min(event_date) as min_date from activity group by player_id
+# Write your MySQL query statement below
+select round(count(a.player_id)/count(*),2) as fraction from(
+    select player_id,min(event_date) as min_date
+    from activity group by player_id
 )f
-left join activity a on f.player_id=a.player_id
-and a.event_date-f.min_date=1;
+left join activity a on a.player_id=f.player_id
+and datediff(a.event_date,f.min_date)=1;
