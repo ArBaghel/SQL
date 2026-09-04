7deb36863b46c7c91807b562e97b0d35dbadeee3
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-09-05 00:49:19 +0530

    Time: 504 ms (64.94%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,5 +1,4 @@
 -- Write your PostgreSQL query statement below
-select distinct(c.customer_id) from customer c 
-left join product p using(product_key)
-group by c.customer_id
-having count(distinct p.product_key)=(select count(*) from product)
+select customer_id from customer  
+group by customer_id
+having count(distinct product_key)=(select count(*) from product)
