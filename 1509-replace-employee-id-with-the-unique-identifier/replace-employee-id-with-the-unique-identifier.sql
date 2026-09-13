526cc56406689526f1d9ea87582786f7ccee2870
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-09-13 11:15:35 +0530

    Time: 1202 ms (83.58%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select eu.unique_id ,e.name from Employees e
-left join EmployeeUNI eu on e.id=eu.id
+select u.unique_id,e.name from employees e
+left join employeeuni u using(id)
