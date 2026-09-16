084c5db6cee98ce4afd6b045e615f9c5cd338744
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-09-16 21:29:24 +0530

    Time: 869 ms (5.71%) | Memory: 0B (100.00%) - LeetSync

--- 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,4 +1,4 @@
--- Write your PostgreSQL query statement below
-select teacher_id,count(distinct subject_id)as cnt from teacher
+# Write your MySQL query statement below
+select teacher_id, count(distinct subject_id) as cnt
+from teacher
 group by teacher_id
-
