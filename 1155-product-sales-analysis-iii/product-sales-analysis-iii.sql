5cf0aae0f594f483393616cf6989799f873606bd
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-09-16 21:43:37 +0530

    Time: 2308 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,3 +1,7 @@
--- Write your PostgreSQL query statement below
-select distinct(product_id), year as first_year,quantity,price from sales
-where (product_id , year) in(select product_id,min(year) as year from sales group by product_id)
+# Write your MySQL query statement below
+select distinct product_id , year as first_year, quantity , price
+from sales 
+where (product_id, year) in (
+    select distinct product_id, min(year) from sales
+    group by product_id
+)
