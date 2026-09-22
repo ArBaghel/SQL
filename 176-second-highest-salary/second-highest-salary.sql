6c1261bbbfe0aa209f789e2e4254ffa54606eccd
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-09-22 22:42:21 +0530

    Time: 456 ms (7.98%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,12 +1,3 @@
 # Write your MySQL query statement below
-select (
-    select  distinct salary  from employee  order by salary desc limit 1 offset 1
-  ) as SecondHighestSalary  ;
-
--- import pandas as pd
-
--- def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
---     s=employee['salary'].drop_duplicates().nlargest(2)
---     if len(s)<2:return pd.DataFrame({'SecondHighestSalary':[None]})
---     return pd.DataFrame({'SecondHighestSalary':[s.iloc[-1]]})
-    
+SELECT max(salary) as SecondHighestSalary
+from employee where salary<(select max(salary) from employee)
